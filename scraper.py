c9fc95ae799e5fa6190689e69b43bd14b66f4ed3
Chai <chairuizhong93@hotmail.com>
2021-03-16 18:40:03 +0800

    scrape

---
+++ scraper.py
@@ -0,0 +1,88 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import telebot
+import schedule, time
+from datetime import datetime, timezone
+
+URLs = {'spot':'https://binance-docs.github.io/apidocs/spot/en/#change-log','futures':'https://binance-docs.github.io/apidocs/futures/en/#change-log','delivery':'https://binance-docs.github.io/apidocs/delivery/en/#change-log'}
+current_date = {'spot':'', 'futures':'', 'delivery':''}
+
+'''
+get the content of the change logs, content ends at the next date
+'''
+def get_current_content(webpage, latest_date_idx, trade_type):
+	content = webpage[latest_date_idx] + ' ' + trade_type.upper()
+	pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
+	for i in range(latest_date_idx + 1, len(webpage) - 1 , 1):
+		if pattern.search(webpage[i]):
+			break
+		content += webpage[i] + '\n'
+	return content
+
+'''
+Update latest update date for future comparison
+'''
+def update_current_date(webpage, latest_date_idx, trade_type):
+	is_updated = False
+	if current_date[trade_type] == '':
+		telebot.send_message("SCRAPER RESTARTED AND UPDATED!")
+		current_date[trade_type] = webpage[latest_date_idx]
+		is_updated = True
+
+	if webpage[latest_date_idx] != current_date[trade_type]:
+		telebot.send_message("NEW UPDATE DETECTED!")
+		is_updated = True
+		current_date[trade_type] = webpage[latest_date_idx]
+
+	return is_updated
+
+def check_updates(webpage, trade_type):
+	webpage = webpage.split('\n')
+	change_log_idx = -1
+
+	'''
+	find index of Change Log keyword, following line is the start of latest updates
+	'''
+	for i in range(len(webpage) - 1, -1, -1):
+		if 'Change Log' in webpage[i]:
+			change_log_idx = i
+			break 
+	if change_log_idx < 0:
+		print(f'change_log_idx: {change_log_idx} is not found!')
+		return
+
+	latest_date_idx = change_log_idx + 1
+	has_update = update_current_date(webpage, latest_date_idx, trade_type)
+	if has_update:
+		content = get_current_content(webpage, latest_date_idx, trade_type)
+		print(content)
+		telebot.send_message(content)
+
+	print(current_date)
+
+def run_scraper():
+	telebot.send_message(f'RUNNING SCRAPER at {datetime.now()}')
+	for trade_type, URL in URLs.items():
+		page = urlopen(URL)
+		html = page.read().decode("utf-8")
+		soup = BeautifulSoup(html, "html.parser")
+		check_updates(soup.get_text(), trade_type)
+
+def heartbeat_check():
+	telebot.send_message(f'HEARTBEAT CHECK at {datetime.now()}')
+
+run_scraper() #run once before scheduled
+schedule.every().hour.do(heartbeat_check)
+schedule.every(4).hours.do(run_scraper)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
+
+
+
+
+
+
+	
---
+++ telebot.py
@@ -0,0 +1,25 @@
+import telegram, json
+import pprint
+import os
+
+bot = telegram.Bot(token=os.environ.get('TELEGRAM_API_KEY'))
+chat_id = os.environ.get('CHAT_ID')
+
+def send_message(message):
+	try:
+		global bot, chat_id
+		# Get your chat id using these lines
+		# updates = json.loads(bot.get_updates()[-1].to_json())
+		# chat_id = updates['message']['chat']['id']
+		# pprint.pprint(updates)
+		# print(chat_id)
+		bot.send_message(chat_id = chat_id, text=message,)
+
+	except Exception as e:
+		bot.send_message(chat_id=chat_id, text='Something went wrong... message:{}'.format(e))
+		print(e)
+
+
+
+if __name__ == '__main__':
+    send_message()
